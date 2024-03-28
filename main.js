d7c6baacb5d7c01dde4661baf4d710ef8b39272d
IronLegends7519 <drafin@edenschool.fr>
2024-03-28 16:02:21 +0100

    finish condition égalité

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 const squares = document.querySelectorAll('.squares')
-
+let player = 1
+let count = 0
 for(let i =0;i<squares.length;i++){
   squares[i].addEventListener('click',()=>{
-    console.log(i)
+    if(count === 8){
+      squares[i].textContent = 'x'
+      setTimeout(()=>{alert('égalité')},500)
+      
+    }
+    else if(squares[i].textContent === ""){
+
+      if(player === 1){
+        squares[i].textContent = "x"
+        player++
+        count++
+        }
+        else{
+          squares[i].textContent = "o"
+          player--
+          count++
+        }
+
+    }
+    
   })
 }
